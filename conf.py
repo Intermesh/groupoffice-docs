de08ed56d617ebce1118fab258181789bd0d1d3c
Merijn Schering <mschering@intermesh.nl>
2019-06-06 15:50:51 +0200

    Updated title

--- conf.py
+++ conf.py
@@ -128,7 +128,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'Group-Office.tex', 'Group-Office installation manual',
+    (master_doc, 'Group-Office.tex', 'Group-Office manual',
      'Merijn Schering', 'manual'),
 ]
 
@@ -138,7 +138,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'group-office', 'Group-Office installation manual',
+    (master_doc, 'group-office', 'Group-Office manual',
      [author], 1)
 ]
 
@@ -149,7 +149,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'Group-Office', 'Group-Office installation manual',
-     author, 'Group-Office', 'One line description of project.',
+    (master_doc, 'Group-Office', 'Group-Office manual',
+     author, 'Group-Office', 'Your online office',
      'Miscellaneous'),
 ]
