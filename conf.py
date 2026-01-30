86cd44af95bc4c4ce0ead245cb1ada63a9d0437d
jadema <ademajoost@hotmail.com>
2026-01-30 10:56:02 +0100

    Update copyright

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Group-Office'
-copyright = '2025, Intermesh BV'
+copyright = '2026, Intermesh BV'
 author = 'Intermesh BV'
 
 # The short X.Y version
